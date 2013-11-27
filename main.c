872ac8b1aa91c1ccdca2390f123fb93ad64a0f42
Leonardo Sampaio <leosfr1419@Leo-Sampaio-MacBook-Air.local>
2013-11-26 22:14:59 -0200

    Adiciona main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  T2
+//
+//  Created by Léo Sampaio on 25/11/13.
+//  Copyright (c) 2013 Léo Sampaio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // Hello test
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+    
+}
+
